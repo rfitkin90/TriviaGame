b4fa0420fb847d8d29592cf963610f92928b07e1
Richard Fitkin <rfitkin90@gmail.com>
2018-11-26 21:50:49 -0800

    finished 11-26 lecture

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,37 +2,34 @@
 $(document).ready(function () {
     // create object of questions with 4 possible answers and one correct answer
 
-    var _questions = {
-        question1: {
-            questionText: "What was the first ever game released for the Atari 2600?",
-            answerA: "Asteroids",
-            answerB: "Snake",
-            answerC: "Pong",
-            answerD: "Space Invaders",
-            correctAnswer: "C",
-            // console: console.log(1),
-        },
-        question2: {
-            questionText: `Which of these composers wrote the soundtracks for the original 
+    var _questions = [{
+        questionText: "What was the first ever game released for the Atari 2600?",
+        answerA: "A. Asteroids",
+        answerB: "B. Snake",
+        answerC: "C. Pong",
+        answerD: "D. Space Invaders",
+        correctAnswer: this.answerC,
+    },
+    {
+        questionText: `Which of these composers wrote the soundtracks for the original 
             "Super Mario Bros." and "The Legend of Zelda" games for the Nintendo Entertainment System?`,
-            answerA: "Koji Kondo",
-            answerB: "Nobuo Uematsu",
-            answerC: "Yasunori Mitsuda",
-            answerD: "Yoko Shimomura",
-            correctAnswer: "A",
-            // console: console.log(2),
-        },
-        question3: {
-            questionText: `Known for making such titles as "World of Warcraft" and "Diablo", the 
+        answerA: "A. Koji Kondo",
+        answerB: "B. Nobuo Uematsu",
+        answerC: "C. Yasunori Mitsuda",
+        answerD: "D. Yoko Shimomura",
+        correctAnswer: this.answerA,
+    },
+    {
+        questionText: `Known for making such titles as "World of Warcraft" and "Diablo", the 
             company "Activision Blizzard" was orginally called by what name in the early 90s?`,
-            answerA: "Interplay Productions",
-            answerB: "Condor Games",
-            answerC: "Ogre Studios",
-            answerD: "Silicon & Synapse",
-            correctAnswer: "D",
-            // console: console.log(3),
-        },
-    };
+        answerA: "A. Interplay Productions",
+        answerB: "B. Condor Games",
+        answerC: "C. Ogre Studios",
+        answerD: "D. Silicon & Synapse",
+        correctAnswer: this.answerD,
+    }];
+
+    // turn this into array with no object names for each element; splice them out
 
     $("#start-button").hover(function () {
         $(this).css("background-color", "#4aaaa5");
@@ -41,12 +38,31 @@ $(document).ready(function () {
     });
 
     // use RNG to select and display a random object with a timer counting down to 0
-    $("#start-button").click(function(){
-    var RNG = Math.floor((Math.random() * 3) + 0);
-    var chosenQuestion = Object.keys(_questions)[RNG];
-    console.log(chosenQuestion);
-    console.log(Object.values(_questions)[RNG]);
+    $("#start-button").click(function () {
+        var RNG = Math.floor((Math.random() * 3) + 0);
+        var chosenQuestion = Object.keys(_questions)[RNG];
+        console.log(chosenQuestion);
+        console.log(Object.values(_questions)[RNG]);
+        $("#start-button").empty();
+        $(".start-row").empty();
+        $("#question-panel").append(_questions[RNG].questionText);
+        $("#question-panel").append(_questions[RNG].answerA);
+        $("#question-panel").append(_questions[RNG].answerB);
+        $("#question-panel").append(_questions[RNG].answerC);
+        $("#question-panel").append(_questions[RNG].answerD);
+        $("#question-panel").css({
+            "background-color": "#191a1e",
+            "border-width": `${2}px`,
+            "border-style": "solid",
+            "border-color": "#777777",
+            "position": "relative",
+            "top": `${135}px`,
+        });
     });
+
+    // ask about global if statements
+    // look up for vs foreach
+    // $('selector').on("click", function () {
 
     // tally # of right/wrong/unselected answers
 
