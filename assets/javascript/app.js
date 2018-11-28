3a0808a38e46ca92ce5b21e477a3d5871f091412
Richard Fitkin <rfitkin90@gmail.com>
2018-11-28 00:07:06 -0800

    finished appending questions and answers after start

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,13 @@
 $(document).ready(function () {
     // create object of questions with 4 possible answers and one correct answer
 
-    var _questions = [{
+    var questionArray = [{
         questionText: "What was the first ever game released for the Atari 2600?",
         answerA: "A. Asteroids",
         answerB: "B. Snake",
         answerC: "C. Pong",
         answerD: "D. Space Invaders",
-        correctAnswer: this.answerC,
+        correctAnswer: "C. Pong",
     },
     {
         questionText: `Which of these composers wrote the soundtracks for the original 
@@ -17,7 +17,7 @@ $(document).ready(function () {
         answerB: "B. Nobuo Uematsu",
         answerC: "C. Yasunori Mitsuda",
         answerD: "D. Yoko Shimomura",
-        correctAnswer: this.answerA,
+        correctAnswer: "A. Koji Kondo",
     },
     {
         questionText: `Known for making such titles as "World of Warcraft" and "Diablo", the 
@@ -26,8 +26,14 @@ $(document).ready(function () {
         answerB: "B. Condor Games",
         answerC: "C. Ogre Studios",
         answerD: "D. Silicon & Synapse",
-        correctAnswer: this.answerD,
+        correctAnswer: "D. Silicon & Synapse",
+        // correctAnswer: this.answerD, // ------------- doesnt work here
+        // correctAnswer: questionArray[2].answerD, // ------------- or here
     }];
+
+    // console.log(questionArray[2].answerD); // but works here
+
+    var chosenQuestion = null;
 
     // turn this into array with no object names for each element; splice them out
 
@@ -38,26 +44,63 @@ $(document).ready(function () {
     });
 
     // use RNG to select and display a random object with a timer counting down to 0
-    $("#start-button").click(function () {
+    $("#start-button").on("click", function () {
         var RNG = Math.floor((Math.random() * 3) + 0);
-        var chosenQuestion = Object.keys(_questions)[RNG];
-        console.log(chosenQuestion);
-        console.log(Object.values(_questions)[RNG]);
+        chosenQuestion = questionArray[RNG];
         $("#start-button").empty();
         $(".start-row").empty();
-        $("#question-panel").append(_questions[RNG].questionText);
-        $("#question-panel").append(_questions[RNG].answerA);
-        $("#question-panel").append(_questions[RNG].answerB);
-        $("#question-panel").append(_questions[RNG].answerC);
-        $("#question-panel").append(_questions[RNG].answerD);
+        $("#question-panel").append(`<p id="question-text">${questionArray[RNG].questionText}</p>`);
+        $("#question-panel").append(`<p id="answer-A" class="answer">${questionArray[RNG].answerA}</p>`);
+        $("#question-panel").append(`<p id="answer-B" class="answer">${questionArray[RNG].answerB}</p>`);
+        $("#question-panel").append(`<p id="answer-C" class="answer">${questionArray[RNG].answerC}</p>`);
+        $("#question-panel").append(`<p id="answer-D" class="answer">${questionArray[RNG].answerD}</p>`);
         $("#question-panel").css({
             "background-color": "#191a1e",
             "border-width": `${2}px`,
             "border-style": "solid",
             "border-color": "#777777",
             "position": "relative",
-            "top": `${135}px`,
+            "top": `${115}px`,
+            "font-weight": "bold",
+            "line-height": 1.3,
+            "padding": `${15}px`,
         });
+        $("#question-panel").children("p").first().css({
+            "margin-bottom": `${20}px`,
+        });
+        $("#question-panel").children(".answer").css({
+            "padding": `${8}px`,
+            "cursor": "pointer",
+        });
+        $("#question-panel").children(".answer").hover(function () {
+            $(this).css("background-color", "#4aaaa5");
+        }, function () {
+            $(this).css("background-color", "#191a1e");
+        });
+        $("#question-panel").children("p").last().css({
+            "margin-bottom": 0,
+        });
+
+        $("#question-panel").append(`<p id="timer">00:25</p>`);
+        $("#timer").css({
+            "color": "#09ff00",
+            "text-align": "center",
+            "margin": `${10}px`,
+            "font-size": `${25}px`,
+        });
+
+        function decrementTimer() {
+
+        };
+
+    });
+
+    $("body").on("click", "p.answer", function () {
+        if ($(this).text() === chosenQuestion.correctAnswer) {
+            console.log("you win, yay");
+        } else { 
+
+        }
     });
 
     // ask about global if statements
